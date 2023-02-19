90208da76c7b5182c02f5c028f0fc10ddc90d530
Surbhi Gulati <surbhigulati2000@gmail.com>
2023-02-18 23:54:34 -0500

    a5: lab completed

--- public/labs/a5/todos/TodoList.js
+++ public/labs/a5/todos/TodoList.js
@@ -1,10 +1,15 @@
+import TodoItem from "./TodoItem.js";
+import todos from "./todos.js";
+
 const TodoList = () => {
    return(`
-       <ul>
-           <li>Buy milk</li>
-           <li>Pickup the kids</li>
-           <li>Walk the dog</li>
-       </ul>
+        <ul>
+        ${
+            todos.map(todo => {
+               return(TodoItem(todo));
+            }).join('')
+         }
+        </ul>
    `);
 }
 export default TodoList;
