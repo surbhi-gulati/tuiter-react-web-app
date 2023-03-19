fdc427e6d13b63f6b1f60a82ef715c7f0b30b1af
Surbhi Gulati <surbhigulati2000@gmail.com>
2023-03-18 23:36:27 -0400

    a7: finish lab

--- src/labs/a7/redux-examples/index.js
+++ src/labs/a7/redux-examples/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import HelloReduxExampleComponent
- from "./hello-redux-example-component";
+import HelloReduxExampleComponent from "./hello-redux-example-component";
 import hello from "./reducers/hello";
 import todos from "./reducers/todos-reducer";
 import {Provider} from "react-redux";
-import { configureStore }
-  from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import Todos from "./todos-component";
+
 const store = configureStore({
  reducer: {hello, todos}
 });
--- src/labs/a7/redux-examples/reducers/todos-reducer.js
+++ src/labs/a7/redux-examples/reducers/todos-reducer.js
@@ -14,8 +14,31 @@ const initialTodos = [
 ];
 
 const todosSlice = createSlice({
-    name: 'todos',
-    initialState: initialTodos,
+  name: 'todos',
+  initialState: initialTodos,
+  reducers: {
+    addTodo(state, action) {
+      state.push({
+        _id: (new Date()).getTime(),
+        do: action.payload.do,
+        done: false
+      });
+    },
+    deleteTodo(state, action) {
+      const index = action.payload
+      state.splice(index, 1)
+    }, 
+    todoDoneToggle(state, action) {
+      const todo = state.find((todo) =>
+            todo._id === action.payload._id)
+      todo.done = !todo.done
+    },  
+  }
 });
 
+export const {
+  addTodo, 
+  deleteTodo, 
+  todoDoneToggle
+} = todosSlice.actions
 export default todosSlice.reducer;
