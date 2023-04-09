4a4618fdf486d3d8e69e75079664e67ad8b1ac64
Surbhi Gulati <surbhigulati2000@gmail.com>
2023-04-09 15:51:31 -0400

    a9: use updated a9 server for service

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE;
+// updated to use the node server deployment var from a9
+const API_BASE = process.env.REACT_APP_API_BASE_A9;
 const TUITS_API = `${API_BASE}/tuits`;
-// const TUITS_API = 'https://tuiter-node-server-app-isp1.onrender.com/api/tuits';
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit);
